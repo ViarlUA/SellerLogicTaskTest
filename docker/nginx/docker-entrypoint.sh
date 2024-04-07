e315dad2b138997027c9d3cd4463b727a6d7d013
kyrylopotashkin <kyrylo.potashkin.dev@gmail.com>
2024-04-07 17:16:35 +0200

    fix docker-entrypoint

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-rm /var/log/nginx/access.log
+if [ -L /var/log/nginx/access.log ]; then
+    echo "Removing symbolic link from /var/log/nginx/access.log"
+    rm /var/log/nginx/access.log
+else
+    echo "/var/log/nginx/access.log is not a symbolic link or does not exist"
+fi
